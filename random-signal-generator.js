84cd3d795eb3f3bba57f25b4a7d39197b295c4eb
Vsevolod <pavlovskyi.ve@gmail.com>
2021-02-28 12:20:32 +0200

    Lab1.2

--- random-signal-generator.js
+++ random-signal-generator.js
@@ -5,7 +5,6 @@ class RandomSignalGenerator {
     this.discreteSamplesNumber = discreteSamplesNumber;
 
     this.signals = [];
-    this.signalsJSON = {};
   }
 
   calculateSignal(amplitude, frequency, time, phase) {
@@ -29,33 +28,52 @@ class RandomSignalGenerator {
 
     return signals;
   }
+}
 
-  generateSignalsJSON() {
-    let signalsJSON = { }
+const mean = (signals) => signals.reduce((prev, curr) => prev + curr, 0) / signals.length
 
-    for (let i = 1; i <= this.harmonicsNumber; i++) {
-      let frequency = (i * this.frequencyCutoff) / this.harmonicsNumber;
+const variance = (signals) => {
+  let Mx = mean(signals)
 
-      let phase = Math.random();
-      let amplitude = Math.random();
+  return signals.map((num) => Math.pow(num - Mx, 2))
+    .reduce((prev, curr) => prev + curr, 0) / (signals.length - 1)
+}
 
-      for (let time = 0; time < this.discreteSamplesNumber; time++) {
-        let signal = this.calculateSignal(amplitude, frequency, time, phase);
-        if (signalsJSON[time] == undefined) { signalsJSON[time] = 0 }
-        signalsJSON[time] += signal;
-      }
+const correlate = (signals1, signals2) => {
+  const n = signals1.length
+  const M1 = mean(signals1)
+  const M2 = mean(signals2)
+
+  let correlation = Array(256).fill(0)
+
+  for (let tau = 0; tau < 256; tau++) {
+    for (let t = 1; t < n - tau; t++) {
+      correlation[tau] += (signals1[t] - M1) * (signals2[t + tau] - M2)
     }
-
-    return JSON.stringify(signalsJSON);
+    correlation[tau] /= (n - 1)
   }
 
-  get mean() {
-    return this.signals.reduce((prev, curr) => prev + curr, 0) / this.signals.length
+  console.log(correlation[0])
+  return correlation
+}
+
+const correlateNormal = (signals1, signals2) => {
+  const n = signals1.length
+  const M1 = mean(signals1)
+  const M2 = mean(signals2)
+
+  const V1 = variance(signals1)
+  const V2 = variance(signals2)
+
+  let correlation = Array(256).fill(0)
+
+  for (let tau = 0; tau < 256; tau++) {
+    for (let t = 1; t < n - tau; t++) {
+      correlation[tau] += (signals1[t] - M1) * (signals2[t + tau] - M2)
+    }
+    correlation[tau] /= Math.sqrt(V1 * V2) * (n - 1)
   }
 
-  get variance() {
-    return this.signals.map((num) => 
-      Math.pow(num - this.mean, 2)
-    ).reduce((prev, curr) => prev + curr, 0) / (this.signals.length - 1)
-  }
+  console.log(correlation[0])
+  return correlation
 }
